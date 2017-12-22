773441b1e13a6c938e4c755d6ec6f362bee22964
Yann <707312973@qq.com>
2017-12-22 15:49:09 +0800

    1

--- load_img.py
+++ load_img.py
@@ -11,26 +11,19 @@ def load_img():
     n = 0
     print('start')
     for matfile in os.listdir(ad_path)[:170]:
-        img_arr[n, :, :, :] = sio.loadmat(matfile)['Y']
+        img_arr[n, :, :, :] = sio.loadmat(os.path.join(ad_path,matfile))['Y']
         n += 1
+        print(n)
     for matfile in os.listdir(mci_path)[:170]:
-        img_arr[n, :, :, :] = sio.loadmat(matfile)['Y']
+        img_arr[n, :, :, :] = sio.loadmat(os.path.join(mci_path,matfile))['Y']
         n += 1
+        print(n)
     for matfile in os.listdir(nc_path)[:170]:
-        img_arr[n, :, :, :] = sio.loadmat(matfile)['Y']
+        img_arr[n, :, :, :] = sio.loadmat(os.path.join(nc_path,matfile))['Y']
         n += 1
+        print(n)
     print("load over")
-    trainImageArray = np.zeros(120*3, 256, 256, 166)
-    testImageArray = np.zeros(50*3, 256, 256, 166)
-
-    trainImageArray[:120, :, :, :] = img_arr[:120, :, :, :]
-    trainImageArray[120:240, :, :, :] = img_arr[170:290, :, :, :]
-    trainImageArray[240:360, :, :, :] = img_arr[340:460, :, :, :]
-    testImageArray[:50, :, :, :] = img_arr[120:170, :, :, :]
-    testImageArray[50:100, :, :, :] = img_arr[290:340, :, :, :]
-    testImageArray[100:150, :, :, :] = img_arr[460:510, :, :, :]
-    np.save('/home/yxq/MRI_IMAGE/trainMRI.npy', trainImageArray)
-    np.save('/home/yxq/MRI_IMAGE/testMRI.npy', testImageArray)
+    np.save('/home/yxq/MRI_IMAGE/DATA_MRI.npy',img_arr)
 
 if __name__ == '__main__':
     load_img()
